1ae4bcd93f76aec9ac206d58f792e66d209ebfb8
Phil Lee <asmega@ph-lee.com>
2014-11-24 16:25:12 +0000

    Submission now sets a created_at timestamp

--- app/models/feedback/submission.rb
+++ app/models/feedback/submission.rb
@@ -4,6 +4,21 @@ module Feedback
   class Submission
     include ::ActiveModel::Model
 
-    attr_accessor :body
+    attr_accessor :body, :created_at
+
+    define_model_callbacks :initialize
+    after_initialize :init_created_at
+
+    def initialize(*args)
+      run_callbacks :initialize do
+        super
+      end
+    end
+
+    private
+
+    def init_created_at
+      self.created_at = Time.now
+    end
   end
 end
