61628c714326abe5c89561afbc172a85b8c3cf5d
Rachel Graves <rachel.graves@moneyadviceservice.org.uk>
2014-11-24 14:31:31 +0000

    Add feedback user story

---
+++ features/step_definitions/feedback_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I visit the feedback tool$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I leave feedback about savings and account creation$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a confirmation message$/) do
+  pending # express the regexp above with the code you wish you had
+end
