0bc6773b286ed659a29ef27294bae22e3cd90842
Phil Lee <asmega@ph-lee.com>
2014-11-28 16:30:39 +0000

    add executable test script

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+set -e
+
+export PATH=./bin:$PATH
+export RAILS_ENV=test
+export BUNDLE_WITHOUT=development
+
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+BUNDLE_JOBS=$((CORES-1))
+
+CI_PIPELINE_COUNTER=${GO_PIPELINE_COUNTER-0}
+CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER-0}
+
+bundle exec bundle install --jobs $BUNDLE_JOBS
+
+bundle exec rspec
+bundle exec cucumber
