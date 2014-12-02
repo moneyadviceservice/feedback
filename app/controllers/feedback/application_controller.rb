eae298cf99203eeac3166c39f1220a8463d18e11
Phil Lee <asmega@ph-lee.com>
2014-12-02 15:48:48 +0000

    implement temporary alternate options helper

--- app/controllers/feedback/application_controller.rb
+++ app/controllers/feedback/application_controller.rb
@@ -1,4 +1,10 @@
 module Feedback
   class ApplicationController < Feedback.parent_controller.constantize
+
+    def alternate_options
+      { }
+    end
+
+    helper_method :alternate_options
   end
 end
