20afa73fd50a94ea11501b90377f10aa5f6c90f6
Phil Lee <asmega@ph-lee.com>
2014-12-03 11:09:15 +0000

    remove temporary code

--- app/controllers/feedback/application_controller.rb
+++ app/controllers/feedback/application_controller.rb
@@ -1,37 +1,5 @@
 module Feedback
   class ApplicationController < Feedback.parent_controller.constantize
-
-    def alternate_options
-      { }
-    end
-
-    helper_method :alternate_options
-
-    def alternate_url
-      ''
-    end
-
-    helper_method :alternate_url
-
-    def alternate_locale
-      :en
-    end
-
-    helper_method :alternate_locale
-
-    def breadcrumbs
-      ::BreadcrumbTrail.home
-    rescue
-      []
-    end
-
-    helper_method :breadcrumbs
-
-    def display_category_directory?
-      false
-    end
-
-    helper_method :display_category_directory?
   end
 end
 
