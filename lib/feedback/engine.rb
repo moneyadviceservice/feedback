3136c40526ab2af9692f7687793c0dd1dc61b28f
Phil Lee <asmega@ph-lee.com>
2014-12-03 11:08:26 +0000

    move parent controller to dummy app initializer

--- lib/feedback/engine.rb
+++ lib/feedback/engine.rb
@@ -1,9 +1,5 @@
 module Feedback
   class Engine < ::Rails::Engine
     isolate_namespace Feedback
-
-    initializer :setup_defaults do |app|
-      Feedback.parent_controller ||= '::ApplicationController'
-    end
   end
 end
