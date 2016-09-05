ec8c947d3172410378c147f2e5aec287e49011c3
Jon Gilbraith <jon@hobocode.io>
2016-09-05 10:34:00 +0100

    `bundle exec bundle install`?

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ BUNDLE_JOBS=$((CORES-1))
 CI_PIPELINE_COUNTER=${GO_PIPELINE_LABEL-0}
 CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER-0}
 
-bundle exec bundle install --jobs $BUNDLE_JOBS
+bundle install --jobs $BUNDLE_JOBS
 
 if [ -n "$GO_PIPELINE_NAME" ]; then
   # set up test DB if running on CI
